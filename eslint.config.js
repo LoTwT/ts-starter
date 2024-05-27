978d5651f3fb5c966d4088af79313aed86f34462
Lo <l709937065@gmail.com>
2024-05-28 01:05:14 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,4 @@
 
 import { defineFlatConfig } from "@ayingott/eslint-config"
 
-export default defineFlatConfig([], {
-  prettier: true,
-  vue: false,
-  unocss: false,
-  react: false,
-})
+export default defineFlatConfig()
