c58726fef0c064f0b533b4c8ce99fcedb8c6d8fa
Lo <l709937065@gmail.com>
2025-02-13 23:50:05 +0800

    chore: update deps and migrate to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from "tsup"
+import { defineConfig } from "tsdown"
 
 export default defineConfig({
   entry: ["./src/index.ts"],
   format: "esm",
   target: "esnext",
   clean: true,
-  cjsInterop: true,
   dts: true,
-  splitting: true,
 })
